39029015abdcb5e723770432a2b0611de94e3a78
Eodera1 <ericowino8@gmail.com>
2024-06-12 06:20:19 +0300

    helloworld

---
+++ helloworld.go
@@ -0,0 +1,11 @@
+// This is a simple hello world program in Go language
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This is a simple hello world program in Go language
+	fmt.Println("Hello, World!")
+}
