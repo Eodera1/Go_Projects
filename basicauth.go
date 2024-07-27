fad0b9c0b05910bdb46fc5e9786820bc5106776b
Eodera1 <ericowino8@gmail.com>
2024-07-27 08:35:40 +0300

    Basic authorization

---
+++ basicauth.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var username string = "wagslane"
+	var password string = "123456789"
+
+	// don't edit below this line
+	fmt.Println("Authorization: Basic", username+":"+password)
+}
